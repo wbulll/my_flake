556c3ed49a5bffa44387f9420e6a19191fc74aaa
System administrator <otoskrupskis01@gmail.com>
2025-11-29 20:45:53 +0200

    test

--- flake.nix
+++ flake.nix
@@ -1,8 +1,8 @@
 {
   description = "A very basic flake";
   inputs = {
-#    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-nixpkgs.url = "github:nixos/nixpkgs?rev=ae814fd3904b621d8ab97418f1d0f2eb0d3716f4"; 
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+#nixpkgs.url = "github:nixos/nixpkgs?rev=ae814fd3904b621d8ab97418f1d0f2eb0d3716f4"; 
 
  };
   outputs = { self, nixpkgs }: {
